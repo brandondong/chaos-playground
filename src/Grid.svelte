61315bac378ac1d1d324b281927424243572bf2a
Brandon <brandondong604@hotmail.com>
2022-01-21 21:28:58 -0800

    Draw grid lines

--- src/Grid.svelte
+++ src/Grid.svelte
@@ -8,12 +8,31 @@
 </script>
 
 <svg viewBox="0 0 {TOTAL_SIZE} {TOTAL_SIZE}">
+    <!-- Start (0, 0) at the top left of the grid.  -->
     <g
         transform="translate({(TOTAL_SIZE - GRID_SIZE) / 2} {(TOTAL_SIZE -
             GRID_SIZE) /
             2})"
     >
-        <line x1={0} y1="0" x2={GRID_SIZE} y2="0" stroke="black" />
+        <!-- Grid lines. -->
+        {#each Array(dimension + 1) as _, i}
+            <line
+                x1={0}
+                y1={i * cellSize}
+                x2={GRID_SIZE}
+                y2={i * cellSize}
+                stroke="black"
+            />
+        {/each}
+        {#each Array(dimension + 1) as _, i}
+            <line
+                x1={i * cellSize}
+                y1={0}
+                x2={i * cellSize}
+                y2={GRID_SIZE}
+                stroke="black"
+            />
+        {/each}
     </g>
 </svg>
 
