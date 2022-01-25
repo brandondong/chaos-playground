e6100cebaaab42483f4b933fa735f6518202a106
Brandon <brandondong604@hotmail.com>
2022-01-24 22:37:23 -0800

    Region line POC

--- src/Grid.svelte
+++ src/Grid.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     const TOTAL_SIZE = 100;
     const GRID_SIZE = 75;
+    const GRID_STROKE = 0.2;
+    const REGION_STROKE = 1;
 
     export let grid;
     $: dimension = Math.sqrt(grid.length);
@@ -22,6 +24,7 @@
                 x2={GRID_SIZE}
                 y2={i * cellSize}
                 stroke="black"
+                stroke-width={GRID_STROKE}
             />
         {/each}
         {#each Array(dimension + 1) as _, i}
@@ -31,8 +34,42 @@
                 x2={i * cellSize}
                 y2={GRID_SIZE}
                 stroke="black"
+                stroke-width={GRID_STROKE}
             />
         {/each}
+        <!-- Outer border. -->
+        <line
+            x1={0}
+            y1={0}
+            x2={GRID_SIZE}
+            y2={0}
+            stroke="black"
+            stroke-width={REGION_STROKE}
+        />
+        <line
+            x1={GRID_SIZE}
+            y1={0}
+            x2={GRID_SIZE}
+            y2={GRID_SIZE}
+            stroke="black"
+            stroke-width={REGION_STROKE}
+        />
+        <line
+            x1={0}
+            y1={GRID_SIZE}
+            x2={GRID_SIZE}
+            y2={GRID_SIZE}
+            stroke="black"
+            stroke-width={REGION_STROKE}
+        />
+        <line
+            x1={0}
+            y1={0}
+            x2={0}
+            y2={GRID_SIZE}
+            stroke="black"
+            stroke-width={REGION_STROKE}
+        />
     </g>
 </svg>
 
